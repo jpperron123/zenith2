d399dd32ecedcd7072a4b5792e9785a2303b7f2d
jpperron123 <jpperron123@hotmail.com>
2025-09-10 10:16:20 -0400

    zenith logo wip

---
+++ resources/Images/logo/zenith_logo.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+    <svg
+        baseProfile="full"
+        height="300"
+        version="1.1"
+        width="300"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:ev="http://www.w3.org/2001/xml-events"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+        <defs />
+        <polygon fill="#0f172a" points="50,220 160,100 140,100 230,40 230,60 170,120 190,120 80,240" />
+        <circle cx="180" cy="80" fill="#0f172a" r="12" />
+        <text fill="#0f172a" font-family="Arial" font-size="32px" font-weight="bold" letter-spacing="4px" x="50" y="285">ZENITH</text>
+    </svg>
+</template>
