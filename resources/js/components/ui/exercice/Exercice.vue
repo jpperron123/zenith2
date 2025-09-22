b045b8841fce68beb051bdf48064251cfda61e0a
jpperron123 <jpperron123@hotmail.com>
2025-09-22 03:18:17 -0400

    Start add an exercice button

---
+++ resources/js/components/ui/exercice/Exercice.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+import Button from '../button/Button.vue';
+
+// Example reactive state
+
+function handleClick() {
+	alert('Button clicked!');
+}
+</script>
+
+<template>
+	<div class="border-2 p-1 bg-black flex flex-col items-center justify-center h-full w-full">
+		<Button @click="handleClick" class="mt-2 cursor-pointer">Add an exercice</Button>
+		<!-- Add your component content here -->
+	</div>
+</template>
